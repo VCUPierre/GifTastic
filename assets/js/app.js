8e50a75e701c009c7182018602c77b0436bea4b4
Pierre R <randalltp@mymail.vcu.edu>
2019-09-14 09:06:57 -0400

    added changes to index and app.js

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var topics = ["dwight schrute", "michael scott", "jim halpert", "pam beesly", "creed bratton"];
+
+function createButtons(intialOrAdd, array){
+    var aTagHolder = $("<a>");
+    
+    if (intialOrAdd === 1){
+        for (let i = 0; i < array.lenght; i++){
+            var gifbutton = $("button",{id: "button" + i});
+            
+        }
+    }
+}
+$("#home").append(JSON.stringify(topics));
