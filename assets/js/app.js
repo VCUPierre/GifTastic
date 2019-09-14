bc622b511087ec7fc1950a533fe6067081dceb89
Pierre R <randalltp@mymail.vcu.edu>
2019-09-14 11:09:46 -0400

    added search field and btn, as well as initial buttons for giftastic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
 var topics = ["dwight schrute", "michael scott", "jim halpert", "pam beesly", "creed bratton"];
 
 function createButtons(intialOrAdd, array){
-    var aTagHolder = $("<a>");
-    
+    var buttonFromDom = $("#buttons");
     if (intialOrAdd === 1){
-        for (let i = 0; i < array.lenght; i++){
-            var gifbutton = $("button",{id: "button" + i});
-            
+        for (let i = 0; i < array.length; i++){
+            let aTagHolder = $("<a>");
+            let gifbutton = $("<button>" , {id: "button" + i, class: "btn btn-success m-2", type: "button", value: i});
+            gifbutton.text(topics[i]);
+            aTagHolder.append(gifbutton);
+            aTagHolder.appendTo(buttonFromDom);
         }
     }
 }
-$("#home").append(JSON.stringify(topics));
+createButtons(1, topics);
+//$("#home").append(JSON.stringify(topics));
